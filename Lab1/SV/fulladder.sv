ea290f1d9fdafe3a4e21b366261a2053235be2b1
Pennylandia12 <158530636+Pennylandia12@users.noreply.github.com>
2024-02-09 12:50:20 -0600

    Add files via upload

--- Lab1/SV/fulladder.sv
+++ Lab1/SV/fulladder.sv
@@ -4,3 +4,12 @@ module fulladder (input  logic a, b, cin, output logic sum, cout);
   assign sum = a ^ b ^ cin;
    
 endmodule
+
+
+module RCA ();
+fulladder fa(a[0] ,b[0] ,cin[0] ,sum[0] , cout[0]);
+fulladder fa2(a[1], b[1], cout[0], sum[1], cout[1]);
+fulladder fa3(a[2], b[2], cout[1], sum[2], cout[2]);
+fulladder fa4(a[3], b[3], cout[2], sum[3], cout[3]);
+
+endmodule
